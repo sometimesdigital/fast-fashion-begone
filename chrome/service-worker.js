47977b89de243dfc5c26429dda845e15a30eb71b
sometimesdigital <dominika@sometimes.digital>
2025-01-04 12:48:58 +0100

    add request to triggers

--- chrome/service-worker.js
+++ chrome/service-worker.js
@@ -1,41 +1,34 @@
-const urls = [
-  "https://www.vinted.pl/api/v2/catalog/items*",
-  "https://www.vinted.pl/api/v2/promoted_closets*",
-  "https://www.vinted.at/api/v2/catalog/items*",
-  "https://www.vinted.at/api/v2/promoted_closets*",
-  "https://www.vinted.be/api/v2/catalog/items*",
-  "https://www.vinted.be/api/v2/promoted_closets*",
-  "https://www.vinted.ca/api/v2/catalog/items*",
-  "https://www.vinted.ca/api/v2/promoted_closets*",
-  "https://www.vinted.cz/api/v2/catalog/items*",
-  "https://www.vinted.cz/api/v2/promoted_closets*",
-  "https://www.vinted.de/api/v2/catalog/items*",
-  "https://www.vinted.de/api/v2/promoted_closets*",
-  "https://www.vinted.es/api/v2/catalog/items*",
-  "https://www.vinted.es/api/v2/promoted_closets*",
-  "https://www.vinted.fr/api/v2/catalog/items*",
-  "https://www.vinted.fr/api/v2/promoted_closets*",
-  "https://www.vinted.hu/api/v2/catalog/items*",
-  "https://www.vinted.hu/api/v2/promoted_closets*",
-  "https://www.vinted.it/api/v2/catalog/items*",
-  "https://www.vinted.it/api/v2/promoted_closets*",
-  "https://www.vinted.lt/api/v2/catalog/items*",
-  "https://www.vinted.lt/api/v2/promoted_closets*",
-  "https://www.vinted.ro/api/v2/catalog/items*",
-  "https://www.vinted.ro/api/v2/promoted_closets*",
-  "https://www.vinted.lu/api/v2/catalog/items*",
-  "https://www.vinted.lu/api/v2/promoted_closets*",
-  "https://www.vinted.se/api/v2/catalog/items*",
-  "https://www.vinted.se/api/v2/promoted_closets*",
-  "https://www.vinted.nl/api/v2/catalog/items*",
-  "https://www.vinted.nl/api/v2/promoted_closets*",
-  "https://www.vinted.sk/api/v2/catalog/items*",
-  "https://www.vinted.sk/api/v2/promoted_closets*",
-  "https://www.vinted.co.uk/api/v2/catalog/items*",
-  "https://www.vinted.co.uk/api/v2/promoted_closets*",
-  "https://www.vinted.com/api/v2/catalog/items*",
-  "https://www.vinted.com/api/v2/promoted_closets*",
+const domains = [
+  "at",
+  "be",
+  "cz",
+  "de",
+  "dk",
+  "es",
+  "fi",
+  "fr",
+  "gr",
+  "hr",
+  "hu",
+  "ie",
+  "it",
+  "lt",
+  "lu",
+  "nl",
+  "pl",
+  "pt",
+  "ro",
+  "se",
+  "sk",
+  "co.uk",
+  "com",
 ];
+
+const urls = domains.flatMap((domain) => [
+  `https://www.vinted.${domain}/api/v2/catalog/items*`,
+  `https://www.vinted.${domain}/api/v2/promoted_closets*`,
+  `https://www.vinted.${domain}/api/v2/feed/events*`,
+]);
 
 const begone = () => {
   chrome.storage.local.get(
@@ -56,19 +49,19 @@ const begone = () => {
               brand.trim().toLowerCase()
             )
           )
-        )
-        .map(
-          (item) =>
-            item.closest("article") ||
-            item.closest(".closet__item") ||
-            item.closest(".closet__item--collage") ||
-            item.closest(".feed-grid__item") ||
-            item.closest(".item-view-items__item")
-        )
-        .forEach((item) => {
-          item.innerHTML = "";
-          item.style.display = "contents";
-        });
+      )
+      .map(
+        (item) =>
+          item.closest("article") ||
+          item.closest(".closet__item") ||
+          item.closest(".closet__item--collage") ||
+          item.closest(".feed-grid__item") ||
+          item.closest(".item-view-items__item")
+      )
+      .forEach((item) => {
+        item.innerHTML = "";
+        item.style.display = "contents";
+      });
     }
   );
 };
